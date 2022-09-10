6ef34d9234865276ae8e1436233c859c0d3a5715
jxnin <jaszczaninmarceli@gmail.com>
2022-09-10 11:56:52 +0200

    tutorial part 2

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,12 @@
+
+from django.http import HttpResponse
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.home, name='home'),
+    path('contact/', views.contact, name="contact"),
+    path('polls/', views.index, name='index'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def contact(request):
+    return HttpResponse("Contact Page")
+
+
+def home(request):
+    return HttpResponse("Home Page")
