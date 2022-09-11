53d77618bbf782e4e3e1d6ddcde2ab6eaf7e0cfd
jxnin <jaszczaninmarceli@gmail.com>
2022-09-11 11:26:00 +0200

    tutorial part 4

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,12 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.http import HttpResponse
-
-
-
-def about(request):
-    return HttpResponse("About page")
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
